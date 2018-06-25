1fdff7f28e25ba0105509564738ece09922c72b2
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-06-25 14:46:27 +0100

    fix sendActivationLink function

--- subModels/Candidate.js
+++ subModels/Candidate.js
@@ -19,62 +19,64 @@ Candidate.add('Candidate', {
 });
 
 //Model Hooks
-Candidate.schema.post('save', function () {
+Candidate.schema.post('save',async function () {
 	if (this.wasNew) {
-		this.sendActivationLink();
+		try {
+			this.sendActivationLink();
+		} catch (e) {
+			console.log(e);
+		}
 	}
 });
 
 // Methods
-Candidate.schema.methods.sendActivationLink = function (callback) {
-	console.log("sending email");
-	if (this.isActivated) {
-		console.log('Account is already activated');
-		return callback(new Error('Account is already activated'));
-	}
+Candidate.schema.methods.sendActivationLink = function () {
+	const user = this;
+	return new Promise(function(resolve, reject) {
+		console.log("sending user activation email");
+		if (user.isActivated) {
+			// console.log('Account is already activated');
+			reject(new Error('Account is already activated'));
+		}
 
-	// if (typeof callback !== 'function') {
-	// 	callback = function (err) {
-	// 		if (err) {
-	// 			console.error('There was an error sending the activation link email:', err);
-	// 		}
-	// 	};
-	// }
-	//
-	// if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-	// 	console.log('Unable to send email - no mailgun credentials provided');
-	// 	return callback(new Error('could not find mailgun credentials'));
-	// }
-	//
-	// const user = this;
-	// const brand = keystone.get('brand');
-	//
-	// const code = jwt.sign({
-	// 	id: this._id,
-	// 	createdAt: Date.now(),
-	// }, process.env.ACTIVATION_JWT_SECRET);
-	// const activationLink = `http://localhost:3000/activate?code=${code}`
-	//
-  // new keystone.Email({
-  //   templateName: 'activate-account',
-  //   transport: 'mailgun',
-  // }).send({
-  //   to: [user.email],
-  //   from: {
-  //     name: 'MCC',
-  //     email: 'no-reply@mycarrerchoice.global',
-  //   },
-  //   subject: 'MCC Account Activation',
-  //   user,
-  //   brand,
-	// 	activationLink
-  // }, callback);
-};
+		if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+			console.log('Unable to send email - no mailgun credentials provided');
+			reject(new Error('could not find mailgun credentials'));
+		}
+
+		const brand = keystone.get('brand');
+
+		const code = jwt.sign({
+			id: user._id,
+			createdAt: Date.now(),
+		}, process.env.ACTIVATION_JWT_SECRET);
+		const activationLink = `http://mycareerchoice.global/activate?code=${code}`
+
+		new keystone.Email({
+			templateName: 'activate-account',
+			transport: 'mailgun',
+		}).send({
+			to: [user.email],
+			from: {
+				name: 'MCC',
+				email: 'no-reply@mycarrerchoice.global',
+			},
+			subject: 'MCC Account Activation',
+			user,
+			brand,
+			activationLink
+		}, (err)=>{
+			if (err) {
+				console.log(err);
+			}
+		});
+	});
+}
 
 /**
  * Relationships
  */
-//Candidate.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
+Candidate.relationship({ ref: 'Payment', path: 'payments', refPath: 'madeBy' });
 
 
 /**
